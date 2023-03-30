56c62ab06fc657512efef2586a221f7ab46f7525
diddnjswns <diddnjswns20@gmail.com>
2023-03-30 18:45:40 +0900

    shoppingmallex 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import {Navbar, Container, Nav} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      hi
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='mainbg'></div>
     </div>
   );
 }
